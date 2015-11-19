5151a4a836dbb361aed6f24b6039a32c28e1971d
megamorf <basti.megamorf+github-com@googlemail.com>
2015-11-19 05:06:31 +0100

    HealthCheck.ps1 clean-up
    applied best practices to the script

--- HealthCheck/HealthCheck.ps1
+++ HealthCheck/HealthCheck.ps1
@@ -7,92 +7,107 @@
 #  Revisions: 
 ##################################################################
 
-#Requires ñVersion 2.0 
-$HOST.UI.RawUI.BackgroundColor  = "DarkBlue"
-$HOST.UI.RawUI.ForegroundColor  = "White"
+#Requires ‚ÄìVersion 2.0
+[CmdletBinding()]
+Param(
+    [Parameter(Position=0,Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [Alias('Instance')]
+    [string] $SQLInstance,
+
+    [Parameter(Position=1,Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [Alias('DBName')]
+    [string] $DatabaseName
+)
+
+# Fail if sqlps module cannot be found (allows use of invoke-sqlcmd)
+try   { Import-Module ‚Äúsqlps‚Äù -DisableNameChecking -ErrorAction stop }
+catch { throw "Exiting because the sqlps module cannot be found" }
    
 #Assign variable values.
-$dir = split-path -Parent $MyInvocation.MyCommand.Path    
-$dir = join-path $dir "\"
-$date = get-date -format "yyyyMMdd"
+$PSScriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Path    
+$dir = Join-Path $PSScriptRoot "\"
+$date = Get-Date -Format "yyyyMMdd"
 
-TRY{
-    $ErrorActionPreference = "Stop";
-     
-    #Replace backslash in instance name.
-    if ($args[0].IndexOf("\") -gt 0)
-      {
-    $SQLInstance = $args[0].replace('\','$')  #Instance name passed as parameter from batch file.
+
+TRY
+{
+    $ErrorActionPreference = "Stop"
+
+    switch ($SQLInstance)
+    {
+        #Replace backslash in instance name.
+        {$_.IndexOf("\") -gt 0} { $SQLInstance = $SQLInstance.replace('\','$') }
+
+        #Replace comma if present due to port number being included.
+        {$_.IndexOf(",") -gt 0} { $SQLInstance = $SQLInstance.replace(',','$') }
+
+        #No replacements required
+        Default { Write-Verbose "No transformations on instance name necessary" }
     }
-    #Replace comma if present due to port number being included.
-    elseif  ($args[0].IndexOf(",") -gt 0)
-      {
-    $SQLInstance = $args[0].replace(',','$')  #Instance name passed as parameter from batch file.
-    }
-    else {$SQLInstance = $args[0]}
 
-    $DatabaseName = $args[1]		          #Database name passed as parameter from batch file.
-    $report = "$dir\Reports\Report_${SQLInstance}_$($args[1])_$date.xlsm"
-    $i = 0
+    $Report = "$dir\Reports\Report_${SQLInstance}_${DatabaseName}_$date.xlsm"
 
-    #Import the sqlps module to allow use of invoke-sqlcmd.    
-    Import-Module ìsqlpsî -DisableNameChecking -WarningAction SilentlyContinue 
-
-    #Check database is online.
     Write-Verbose 'Checking database is online.'
     
-    $dbState = invoke-sqlcmd ñServerInstance $args[0] -Query "SELECT state_desc FROM sys.databases WHERE name = '$DatabaseName';"  | %{'{0}' -f $_[0]}
+    $dbState = invoke-sqlcmd ‚ÄìServerInstance $SQLInstance -Query "SELECT state_desc FROM sys.databases WHERE name = '$DatabaseName';"  | %{'{0}' -f $_[0]}
     if ($dbState -ne "online")
-      {
-      "The database '$DatabaseName' is in $dbState mode. Please bring database online and try again."
-      sleep -s 5
-      return
-      }
+    {
+        "The database '$DatabaseName' is in $dbState mode. Please bring database online and try again."
+        sleep -s 5
+        return
+    }
 
     #Get SQL Server version and compatibility level of target database.
-    [int]$compLevel = invoke-sqlcmd ñServerInstance $args[0] -Query "SELECT compatibility_level FROM sys.databases WHERE name = '$DatabaseName';"  | %{'{0}' -f $_[0]}
-    [int]$ver = invoke-sqlcmd ñServerInstance $args[0] -Query "SELECT REPLACE(LEFT(CONVERT(varchar, SERVERPROPERTY ('ProductVersion')),2), '.', '');"  | %{'{0}' -f $_[0]}
+    [int]$compLevel = invoke-sqlcmd ‚ÄìServerInstance $SQLInstance -Query "SELECT compatibility_level FROM sys.databases WHERE name = '$DatabaseName';"  | %{'{0}' -f $_[0]}
+    [int]$ver = invoke-sqlcmd ‚ÄìServerInstance $SQLInstance -Query "SELECT REPLACE(LEFT(CONVERT(varchar, SERVERPROPERTY ('ProductVersion')),2), '.', '');"  | %{'{0}' -f $_[0]}
 
     #Set folder path depending upon SQL version.
-    if ($ver -eq 9) {$FolderPath = "$dir\Scripts_2005\"}	
-        elseif ($ver -eq 10) {$FolderPath = "$dir\Scripts_2008\"}	
-        elseif ($ver -eq 11) {$FolderPath = "$dir\Scripts_2012\"}	
-        elseif ($ver -eq 12) {$FolderPath = "$dir\Scripts_2014\"}	
-         
-    $files = get-childitem  -path $FolderPath -filter "*.sql"
-    $count = $files.length
+    switch ($ver)
+    {
+        9  { $FolderPath = "$dir\Scripts_2005\"; break }
+        10 { $FolderPath = "$dir\Scripts_2008\"; break }
+        11 { $FolderPath = "$dir\Scripts_2012\"; break }
+        12 { $FolderPath = "$dir\Scripts_2014\"; break }
+        Default { throw "Unknown SQL Server version. Exiting" }
+    }
 
     #Run server information script.
     Write-Verbose 'Getting server information.'
-    
-    powershell.exe -file "$FolderPath\ServerInformation.ps1" $args[0], $dir
+    powershell.exe -file "$FolderPath\ServerInformation.ps1" $SQLInstance $PSScriptRoot
+
+    $Files = Get-ChildItem  -Path $FolderPath -Filter "*.sql" | Sort-Object
+    $Count = $Files.length
 
     #Loop through the .sql files and run them.
-    foreach ($filename in $files | sort-object)
-      { 
-    	$i++
-    	$outfile = "$dir\Reports\Temp\" + $filename.Name.Replace(".sql", "") + ".csv"
+    foreach ($Filename in $Files)
+    { 
+        $i++
+        $Outfile = Join-Path "$dir\Reports\Temp\"  $Filename.Name.Replace(".sql", ".csv")
 
-    	invoke-sqlcmd ñServerInstance $args[0] -Database $args[1] -InputFile $filename.Fullname |Export-Csv -Path $outfile -NoTypeInformation
-    	Write-Progress -activity "Exporting $filename to CSV. File $i/$count." -status "Completed: " -PercentComplete (($i/$count)*100)       
+        invoke-sqlcmd ‚ÄìServerInstance $SQLInstance -Database $DatabaseName -InputFile $Filename.Fullname | Export-Csv -Path $Outfile -NoTypeInformation -Encoding UTF8
+        Write-Progress -activity "Exporting $Filename to CSV. File $i/$Count." -status "Completed: " -PercentComplete (($i/$Count)*100)       
     }
 
     #For SQL 2005/2008 compatibility 80 databases, run scripts that use CROSS APPLY or UNPIVOT against the master database.
-    if ($ver -eq 9 -OR $ver -eq 10 )
-    {$FolderPath = "$FolderPath\80\"
-     $files = get-childitem -path $FolderPath -filter "*.sql"
-        foreach ($filename in $files | sort-object)
-          {
-        	$outfile = "$dir\Reports\Temp\" + $filename.Name.Replace(".sql", "") + ".csv"
-        	invoke-sqlcmd ñServerInstance $args[0] -Database "master" -InputFile $filename.Fullname -Variable "DB='${DatabaseName}'"|Export-Csv -Path $outfile -NoTypeInformation       
+    if ( ($ver -eq 9) -OR ($ver -eq 10) )
+    {
+        $FolderPath = "$FolderPath\80\"
+        $Files = Get-ChildItem -Path $FolderPath -Filter "*.sql" | Sort-Object
+
+        foreach ($Filename in $Files)
+        {
+            $Outfile = Join-Path "$dir\Reports\Temp\"  $Filename.Name.Replace(".sql", ".csv")
+            invoke-sqlcmd ‚ÄìServerInstance $SQLInstance -Database "master" -InputFile $Filename.Fullname -Variable "DB='${DatabaseName}'" | Export-Csv -Path $Outfile -NoTypeInformation -Encoding UTF8   
         }
     }
 
     #Create report file.
-    copy-item "$dir\Reports\Master.xlsm" -Destination $report -force 
+    Copy-Item "$dir\Reports\Master.xlsm" -Destination $Report -Force 
 
     #Open report file.
-    $excel = new-object -comobject excel.application
+    $excel = New-Object -ComObject excel.application
     $excel.Visible = $False
     $workbook = $excel.workbooks.open($report)   
     $worksheet = $workbook.worksheets.item(1)
@@ -111,10 +126,11 @@ TRY{
     $excel = $Null
      
     #Remove CSVs.
-    get-childitem -path $dir\Reports\Temp\ -recurse -filter "*.csv" | remove-item
+    Get-ChildItem -Path "$dir\Reports\Temp\" -Recurse -Filter "*.csv" | Remove-Item
     
-  }
-CATCH {
-	get-childitem -path $dir\Reports\Temp\ -recurse -filter "*.csv" | remove-item
-        Write-output "$(Get-Date ñf G) - Error:$filename - $($_.Exception.Message)" | out-file -filepath "$dir\ErrorLog.txt" -append
-    }
+}
+CATCH 
+{
+    Get-ChildItem -Path "$dir\Reports\Temp\" -Recurse -Filter "*.csv" | Remove-Item
+    Write-output "$(Get-Date ‚Äìf G) - Error:$Filename - $($_.Exception.Message)" | Out-File -FilePath "$dir\ErrorLog.txt" -Append
+}
